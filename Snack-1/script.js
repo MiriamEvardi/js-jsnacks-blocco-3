7ad1b941342864801167e99326b24b59271bf2d5
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-12 12:11:32 +0100

    snack-1

--- Snack-1/script.js
+++ Snack-1/script.js
@@ -1,1 +1,14 @@
-console.log("ciao")
+//Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+
+function reverseString(word) {
+    let reverse = "";
+
+    for (let i = word.length - 1; i >= 0; i--) {
+
+        reverse += word[i];
+    }
+    document.getElementById("word").innerText = reverse;
+}
+
+
+reverseString("Parallelepipedo");
